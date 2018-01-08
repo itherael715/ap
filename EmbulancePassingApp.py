1c878136cf5ef05f331ccb38c4f8c7b315e4eb5b
itherael715 <33441536+itherael715@users.noreply.github.com>
2018-01-09 07:34:34 +0800

    Update EmbulancePassingApp.py

--- EmbulancePassingApp.py
+++ EmbulancePassingApp.py
@@ -1,29 +1,29 @@
 #-*- coding: utf-8 -*-
+
 import kivy
+import datetime
 from kivy.app import App
 from kivy.uix.button import Label
 from kivy.uix.widget import Widget
-from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.listview import ListItemButton
 from kivy.uix.textinput import TextInput
-from kivy.properties import ObjectProperty
-from kivy.base import runTouchApp
 from kivy.core.text import LabelBase
 import kivy.resources
 from kivy.core.window import Window
-from kivy.uix.popup import Popup
- 
+font_name = kivy.resources.resource_find('DroidSansFallback.ttf')
+
 class Input_Box_Layout(BoxLayout):
     pass
- 
+
 class EmbulancePassingApp(App):
     def build(self):
         Window.clearcolor = (.85, .85, .85, .3)
-        textinput = TextInput()
-        textinput.bind()
-        return Input_Box_Layout()
+            
+    def buttonClicked(self,button):
+        pass
     
-EP_app = EmbulancePassingApp()
-EP_app.run()
+        
+if __name__ == "__main__":
+    EmbulancePassingApp().run()
